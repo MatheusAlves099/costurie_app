9c72f07c1bb55e832e5ba2aab70c7f2c408895db
ALEXSSANDRO DA SILVA GOMES <22282169@dsn127m1249387.cfp127aluno.local>
2023-10-25 16:55:29 -0300

    colocando loading nas telas

--- app/src/main/java/br/senai/sp/jandira/costurie_app/screens/explore/ExploreScreen.kt
+++ app/src/main/java/br/senai/sp/jandira/costurie_app/screens/explore/ExploreScreen.kt
@@ -19,7 +19,6 @@ import androidx.compose.material.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
